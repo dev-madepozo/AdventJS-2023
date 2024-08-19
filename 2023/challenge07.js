cf87b4bf25e9c826fd9d41e4228910d3361780c0
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-08-19 14:34:38 -0500

    Update solution for challenge 07

--- 2023/challenge07.js
+++ 2023/challenge07.js
@@ -1,6 +1,6 @@
 /*
   Challenge # 07
-  Title: The 3D boxes
+  Title: 📦 The 3D boxes
   Level: Easy
   Link: https://adventjs.dev/en/challenges/2023/7
 */
@@ -8,19 +8,15 @@
 function drawGift(size, symbol) {
   if (size === 1) return '#\n'
   const max = size * 2 - 1
-  const draff = [' '.repeat(max - size) + '#'.repeat(size)]
+  const drawing = ['#'.repeat(size).padStart(max, ' ') + '\n']
 
-  for (let i = 0; i < size - 2; i++) {
-    const line = '#' + symbol.repeat(size -2 ) + '#' + symbol.repeat(i) + '#'
-    draff.push(' '.repeat(max - line.length) + line)
+  for (const i in '#'.repeat(size - 2)) {
+    let line = `#${symbol.repeat(size - 2)}#${symbol.repeat(i)}#\n`
+    drawing[~~i + 1] = line.padStart(max + 1, ' ')
+    drawing[max - i - 2] = line
   }
 
-  draff.push('#'.repeat(size) + symbol.repeat(size - 2) + '#')
-
-  for (let i = size - 3; i >= 0; i--) {
-    draff.push('#' + symbol.repeat(size -2 ) + '#' + symbol.repeat(i) + '#')
-  }
-
-  draff.push('#'.repeat(size) +'\n')
-  return draff.join('\n')
+  drawing[size - 1] = '#'.repeat(size) + symbol.repeat(size - 2) + '#\n'
+  drawing[max - 1] = '#'.repeat(size) + '\n'
+  return drawing.join('')
 }
