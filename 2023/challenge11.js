63c7259b8a7e1c1489eb57105c71948c67cc9168
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-08-21 11:22:14 -0500

    📖 The studious elves

--- 2023/challenge11.js
+++ 2023/challenge11.js
@@ -5,17 +5,15 @@
   Link: https://adventjs.dev/en/challenges/2023/11
 */
 
-function getIndexsForPalindrome(word) {
+function getIndexsForPalindrome2(word) {
   if (word == word.split('').reverse().join('')) return []
 
-  for (let i = 0; i < Math.round(word.length / 2) ; i++) {
-    if (word[i] == word[word.length-i-1] && i != word.length-i-1) continue
+  for (let i = 0; i < Math.round(word.length / 2); i++) {
     for (let j = i + 1; j < word.length; j++) {
-      if (word[i] === word[j]) continue
       const temp = word.split('')
       temp[i] = word[j]
       temp[j] = word[i]
-      if (temp.join('') === temp.reverse().join('')) return [i, j]
+      if (temp.toString() === temp.reverse().toString()) return [i, j]
     }
   }
 
