687d963096e70ae60351fdd7b063caec5c54948f
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-08-21 17:49:06 -0500

    🚨 Autonomous robot

---
+++ 2023/challenge14.js
@@ -0,0 +1,16 @@
+/*
+  Challenge # 14
+  Title: 🚨 Autonomous robot
+  Level: Medium
+  Link: https://adventjs.dev/en/challenges/2023/14
+*/
+
+function maxGifts(houses) {
+  let [total, previous] = [0, 0]
+
+  for (const value of houses) {
+    [previous, total] = [total, Math.max(total, previous + value)]
+  }
+
+  return total
+}
