5af6db63f5964f94b84a187d9a883f4593127d90
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-08-25 01:04:56 -0500

    🗺️ Calculating distances

--- 2023/challenge25.js
+++ 2023/challenge25.js
@@ -6,22 +6,20 @@
 */
 
 function travelDistance(map) {
-  map = map.replace('S', '0')
-  const positions = {}
+  map = map.split('\n')
+  const size = map[0].length
+  const newMap = map.join('');
+  const sIndex = newMap.indexOf('S')
+  let [santaX, santaY] = [sIndex / size | 0, sIndex % size]
+  let ditance = 0
+  const totalChidren = newMap.match(/\d/g).length
 
-  for (const [i, row] of map.split('\n').entries()) {
-    for (const pos of [...row.matchAll(/\d/gi)]) {
-      positions[pos[0]] = [i, pos.index]
-    }
+  for (const i of Array(totalChidren).keys()) {
+    const cIndex = newMap.indexOf(i + 1)
+    const childPos = [cIndex / size | 0, cIndex % size]
+    ditance += Math.abs(santaX - childPos[0]) + Math.abs(santaY - childPos[1]);
+    [santaX, santaY] = childPos
   }
 
-  let [sX, sY] = positions['0']
-  let distance = 0
-
-  for (const [x, y] of Object.values(positions)) {
-    distance += Math.abs(x - sX) + Math.abs(y - sY);
-    [sX, sY] = [x, y]
-  }
-
-  return distance
+  return ditance
 }
