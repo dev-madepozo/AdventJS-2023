38464b01a119afab63b9aed4039e632d925ed85f
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-08-26 23:12:29 -0500

    ↔️ Autonomous robot

--- 2023/challenge15.js
+++ 2023/challenge15.js
@@ -1,6 +1,6 @@
 /*
   Challenge # 15
-  Title: Autonomous robot
+  Title: ↔️ Autonomous robot
   Level: Medium
   Link: https://adventjs.dev/en/challenges/2023/15
 */
@@ -9,18 +9,27 @@ function autonomousDrive(store, movements) {
   let x = store.findIndex(row => row.includes('!'))
   let y = store[x].indexOf('!')
 
+  const rightOrLeft = {
+    R: ['!.', '.!'],
+    L: ['.!', '!.']
+  }
+
+  const diretions = { R: 1, D: 1, L: -1, U: -1 }
+
   for(const mov of movements) {
-    const n = 'RD'.indexOf(mov) || 1
+    const n = diretions[mov]
 
-    if ('RL'.includes(mov) && store[x][y + n] == '.') {
-      store[x] = store[x].replace(...(mov == 'R' ? ['!.', '.!'] : ['.!', '!.']))
-      y += n
-    }
-
-    if ('UD'.includes(mov) && store[x + n] && store[x + n][y] == '.') {
-      store[x + n] = store[x + n].substr(0, y) + '!' + store[x + n].substr(y+1)
-      store[x] = store[x].replace('!', '.')
-      x += n
+    if ('RL'.includes(mov)) {
+      if (store[x][y + n] == '.') {
+        store[x] = store[x].replace(...(rightOrLeft[mov]))
+        y += n
+      }
+    } else {
+      if (store[x + n]?.[y] == '.') {
+        store[x+n] = store[x+n].substr(0, y) + '!' + store[x+n].substr(y+1)
+        store[x] = store[x].replace('!', '.')
+        x += n
+      }
     }
   }
 
